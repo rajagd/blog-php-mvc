d812b9dc3a39e38fbc91e1576f3116336d023736
rajagd <71009166+rajagd@users.noreply.github.com>
2020-09-23 23:09:51 +0200

    créer la class Manager
    créer la class Manager pour se connecter à la base de donnée

---
+++ model/Manager.php
@@ -0,0 +1,30 @@
+<?php
+class Manager
+{
+    //Nos constantes
+
+    const DB_HOST = 'mysql:host=localhost;dbname=jeansblog;charset=utf8';
+    const DB_USER = 'root';
+    const DB_PASS = 'root';
+
+    //Méthode de connexion à notre base de données
+    
+    public function getConnection()
+    {
+        //Tentative de connexion à la base de données
+
+        try {
+            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            //On renvoie la connexion
+            return $connection;
+            
+        } // On lève une erreur si la connexion échoué
+
+        catch (Exception $e) {
+            die('Erreur : ' .$e->getMessage());
+        }
+
+    }
+}
